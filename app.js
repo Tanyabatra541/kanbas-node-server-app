6e6960eeead4209fc4e1a1d6fd7d85b87ba82e23
Tanya Batra <tanyabatra@Tanyas-MacBook-Pro.local>
2023-11-19 18:50:19 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import cors from "cors";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(cors);
+CourseRoutes(app);
+ModuleRoutes(app);
+Hello(app);
+Lab5(app);
+app.listen(process.env.PORT || 4000);
