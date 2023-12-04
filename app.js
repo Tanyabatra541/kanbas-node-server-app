5bc0e76df1279e5a605041db922ecce8bf4dcddb
Tanya Batra <tanyabatra@Tanyas-MacBook-Pro.local>
2023-12-04 18:55:40 -0500

    added sameSite as lax

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import mongoose from "mongoose";
 const CONNECTION_STRING =
   process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
 mongoose.connect(CONNECTION_STRING);
+console.log(CONNECTION_STRING);
 // mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 import courseRoutes from "./courses/routes.js";
 import moduleRoutes from "./modules/routes.js";
@@ -31,6 +32,12 @@ if (process.env.NODE_ENV !== "development") {
   sessionOptions.cookie = {
     sameSite: "none",
     secure: true,
+  };
+}
+else{
+  sessionOptions.cookie = {
+    sameSite: "lax",
+    secure: false,
   };
 }
 app.use(session(sessionOptions));
