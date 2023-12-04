558c4707c3bed0ab3814124c5b0bcb9ba04a9677
Tanya Batra <tanyabatra@Tanyas-MacBook-Pro.local>
2023-12-04 03:50:24 -0500

    a6

---
+++  ~/.bash_profile
@@ -0,0 +1,3 @@
+NODE_ENV=development
+FRONTEND_URL=http://localhost:3000
+DB_CONNECTION_STRING="mongodb+srv://tanyabatra:tanya123@kanbascluster.bdhhqbr.mongodb.net/?retryWrites=true&w=majority"
--- app.js
+++ app.js
@@ -1,16 +1,43 @@
-import express from 'express';
+import express from "express";
 import hello from "./hello.js";
 import lab5 from "./lab5.js";
 import cors from "cors";
+import UserRoutes from "./users/routes.js";
+import mongoose from "mongoose";
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
+// mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 import courseRoutes from "./courses/routes.js";
 import moduleRoutes from "./modules/routes.js";
 import "dotenv/config";
+import session from "express-session";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
 courseRoutes(app);
 moduleRoutes(app);
 hello(app);
 lab5(app);
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
